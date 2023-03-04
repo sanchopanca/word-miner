d815542ecd8bd1c0b010642c6afff1ac70fc63d7
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2023-03-04 13:16:40 +0100

    Add `--beginning` to letter-combinations
    Useful when you want to train typing beginning of words

--- word_miner/letter_combinations.py
+++ word_miner/letter_combinations.py
@@ -12,12 +12,30 @@ def find_combinations(file, length) -> Counter:
     return combinations
 
 
+def find_combinations_from_beginning(file, length) -> Counter:
+    combinations = Counter()
+    for word in file:
+        word = word.strip().strip(".-")
+        combinations[word[:length]] += 1
+    return combinations
+
+
 @click.command()
 @click.argument("dictionary", type=click.File())
 @click.option("--length", "-l", default=2, help="length of the combinations")
 @click.option("--top", "-t", default=5, help="number of the most common combinations")
-def report_combinations(dictionary, length, top):
-    combinations = find_combinations(dictionary, length)
+@click.option(
+    "--beginning",
+    "-b",
+    is_flag=True,
+    default=False,
+    help="show only combinations from the beginning of the word",
+)
+def report_combinations(dictionary, length, top, beginning):
+    if beginning:
+        combinations = find_combinations_from_beginning(dictionary, length)
+    else:
+        combinations = find_combinations(dictionary, length)
     for combination, count in combinations.most_common(top):
         print(f"{combination}: {count}")
 
